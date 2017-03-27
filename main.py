79f3a9f9e387ff148176677d96157b21672c316f
Rina <schiller.rina@gmail.com>
2017-03-26 23:19:05 -0400

    bug fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask.ext.socketio import SocketIO, emit
+# from flask.ext.socketio import SocketIO, emit
 import cgitb
 import os
 from TwitterSearch import *
