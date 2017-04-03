fc7997a041e207e9eefe88f7088194e62eae4ae0
Your Name <you@example.com>
2017-04-03 15:44:49 -0400

    changed tweet formatting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 import cgitb
 import os
 from TwitterSearch import *
+import datetime
 
 cgitb.enable()
 
@@ -15,12 +16,13 @@ app = Flask(__name__)
 @app.route('/') #root directory, home page of website, called a decorator
 def index():
     tweets = []
-
     try:
         tso = TwitterSearchOrder() # create a TwitterSearchOrder object
         tso.set_keywords(['clinton', 'bitch']) # all the terms to search for
         tso.set_language('en') 
+        tso.set_count(1)
         tso.set_include_entities(False)
+        
 
         ts = TwitterSearch(
             consumer_key = 'JPIQgfrt5gTI90PgC2DNoLf44',
@@ -31,10 +33,16 @@ def index():
 
         # main part
         for tweet in ts.search_tweets_iterable(tso):
-           # tweets.append('@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ))
-           tweets.append(tweet['text'])
+         tweets.append({'text': tweet['text'],
+                         'date': tweet['created_at'],
+                         'name': tweet['user']['name'],
+                         'screen_name': tweet['user']['screen_name'],
+                         'RT': tweet['retweeted']})
+#         if tweet.has_key('retweeted_status'):   
+#             tweets.append({'text2': tweet['retweeted_status']['text']})
+         print(tweets)
     except TwitterSearchException as e: # take care of all those ugly errors if there are some
-        print(e)
+         print(e)
 
     return render_template("index.html", tweets = tweets)
 
@@ -50,4 +58,4 @@ def tweets():
 if __name__ == "__main__": #only start web server if this file is called directly  
 #    socketio.run(app)
     port = int(os.environ.get('PORT', 5000)) 
-    app.run(debug=True, host='0.0.0.0', port=port) #starts app on web server 
+    app.run(debug=True, host='127.0.0.1', port=port) #starts app on web server 
