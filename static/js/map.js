c28663da05ca044b29dd43450ef0333ec2fda8eb
Rina <schiller.rina@gmail.com>
2017-05-02 13:02:05 -0400

    added more dummy data

--- static/js/map.js
+++ static/js/map.js
@@ -1,3 +1,4 @@
+'''map'''
 function init_map() {
   var minZoomLevel = 4
   var var_location = new google.maps.LatLng(37.09024, -95.712891);
@@ -18,25 +19,13 @@ function init_map() {
   }
 google.maps.event.addDomListener(window, 'load', init_map);
 
-// $(document).ready(function(){     
-    // namespace = '/tweets';
-    // var socket = io.connect('//' + document.domain + ':' + location.port + namespace);
-    // socket.on('connect', function(){
-    //     console.log('user connected on client side');
-    // })
-
-    // //on receiving tweet 
-    // socket.on('tweet', function(msg) {
-    //     var date = msg.created_at;
-    //     var text = msg.text;
-    //     console.log(msg)
-    //     console.log(msg.retweeted_status.place.full_name)
-    //     console.log(msg.retweeted_status.place.bounding_box.coordinates)
-    //     }
-        
-    //     //prepend tweet to tweetArea
-    //     $('<div><a href="https://twitter.com/' + msg.user.screen_name + '" target="_blank"><img id="profimg" src="' + msg.user.profile_image_url_https + '"</a><div>' + msg.user.name + '<a href="https://twitter.com/' + msg.user.screen_name + '" target="_blank"> @' + msg.user.screen_name + '</a></div><div id="text"> ' + text + '</div><br><div id="date">' + date.substring(0,date.length-14) + '</div></div>').hide().prependTo('#tweetArea').fadeIn('slow');    
-    //     $('#tweetArea').linkify({
-    //       target: "_blank"    
-    //     });
-// });
+'''SocketIO'''
+// start up the SocketIO connection the server
+  namespace = '/tweets';
+  var socket = io.connect('//' + document.domain + ':' + location.port + namespace);
+  socket.on('connect', function(){
+      console.log('user connected on map client side');
+  })
+  // socket.on('tweet', function(msg) {
+  //   console.log(msg)
+  // }
